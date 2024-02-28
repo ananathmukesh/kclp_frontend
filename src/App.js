88080d20ab379308d968c723e2cf9f65cf18c2a6
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-02-28 11:37:27 +0530

    push login commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
      
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/main" element={<Main />}>
