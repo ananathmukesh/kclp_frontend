4f8bace41a582646b2d329f11cb32dbb3ed0bef3
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-03-19 20:15:02 +0530

    SMALL DATA

--- src/config/serverUrl.js
+++ src/config/serverUrl.js
@@ -1,9 +1,22 @@
 // in a file named apiConfig.js
 
-const Nodeapi = 'http://localhost:8001/api';
-const ResumeApi = 'http://localhost:8001/assets/UserResume/';
-const ProfileImgApi = 'http://localhost:8001/assets/UserProfileImage/';
-const authapi = 'http://localhost:8001';
+// const Nodeapi = 'http://localhost:8001/api';
+// const ResumeApi = 'http://localhost:8001/assets/UserResume/';
+// const ProfileImgApi = 'http://localhost:8001/assets/UserProfileImage/';
+// const authapi = 'http://localhost:8001';
+// module.exports = {
+//   Nodeapi,
+//   ResumeApi,
+//   authapi,
+//   ProfileImgApi,
+  
+// };
+
+
+const Nodeapi = 'https://www.kodukku.com/api';
+const ResumeApi = 'https://www.kodukku.com/assets/UserResume/';
+const ProfileImgApi = 'https://www.kodukku.com/assets/UserProfileImage/';
+const authapi = 'https://www.kodukku.com';
 module.exports = {
   Nodeapi,
   ResumeApi,
@@ -11,3 +24,4 @@ module.exports = {
   ProfileImgApi,
   
 };
+
