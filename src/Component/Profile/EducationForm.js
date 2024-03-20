5fea6fc3cbe87f3c9d0def58e9ea5e1573fc0014
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-03-20 17:33:24 +0530

    preview data

--- src/Component/Profile/EducationForm.js
+++ src/Component/Profile/EducationForm.js
@@ -70,27 +70,11 @@ const EducationForm = () => {
 
 
   const handleAddInputField = () => {
-    // Check if prevData is an array
-    if (!Array.isArray(prevData)) {
-      // If not, initialize it as an empty array
-      setTableData([{ 
-        id: 1, 
-        scl_qualification: "",
-        scl_specialization: "",
-        scl_start: "",
-        scl_end: "",
-        scl_name: "",
-        scl_percentage: "",
-        scl_section:""
-      }]);
-      return; // Return early to avoid further execution
-    }
-  
     // Add a new row with empty input fields
-    setTableData((prevData) => [
-      ...prevData,
+    setTableData((prevTableData) => [
+      ...prevTableData,
       { 
-        id: prevData.length + 1, 
+        id: prevTableData.length + 1, 
         scl_qualification: "",
         scl_specialization: "",
         scl_start: "",
@@ -101,6 +85,7 @@ const EducationForm = () => {
       },
     ]);
   };
+  
   
 
   const handleRemoveRow = (index) => {
